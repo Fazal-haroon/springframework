f4839b23c8c376ddb070b2bee0d2016045859189
Fazal Haroon <kharoon187@gmail.com>
2023-01-07 03:43:57 +0400

    slf4j not work, fixing minor stuff

--- src/main/java/com/example/demo/SpringIn5StepBasicApp.java
+++ src/main/java/com/example/demo/SpringIn5StepBasicApp.java
@@ -21,16 +21,20 @@ public class SpringIn5StepBasicApp {
 //		System.out.println("i = " + i);
 
 //		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepBasicApp.class, args);
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepBasicApp.class);
-		BinarySearchImpl bean = applicationContext.getBean(BinarySearchImpl.class);
-		System.out.println("bean.hashCode() = " + bean.hashCode());
-		System.out.println("bean = " + bean);
-		BinarySearchImpl bean2 = applicationContext.getBean(BinarySearchImpl.class);
-		System.out.println("bean2.hashCode() = " + bean2.hashCode());
-		System.out.println("bean2 = " + bean2);
-		int i = bean.binarySearch(new int[]{12, 42, 11, 1}, 3);
-		System.out.println("i = " + i);
+		try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepBasicApp.class)){
+			BinarySearchImpl bean = applicationContext.getBean(BinarySearchImpl.class);
+			System.out.println("bean.hashCode() = " + bean.hashCode());
+			System.out.println("bean = " + bean);
+			BinarySearchImpl bean2 = applicationContext.getBean(BinarySearchImpl.class);
+			System.out.println("bean2.hashCode() = " + bean2.hashCode());
+			System.out.println("bean2 = " + bean2);
+			int i = bean.binarySearch(new int[]{12, 42, 11, 1}, 3);
+			System.out.println("i = " + i);
+		}
+
 		//Application Context will maintain all the beans
+
+//		applicationContext.close();
 
 	}
 
