1c61673f46215d65553d8d7e074d1100a166efc6
Fazal Haroon <kharoon187@gmail.com>
2023-01-07 03:25:57 +0400

    spring boot migrate into spring core framework

--- src/main/java/com/example/demo/SpringIn5StepComponentScanApp.java
+++ src/main/java/com/example/demo/SpringIn5StepComponentScanApp.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import com.example.componentscan.ComponentDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.example.demo")
 //@ComponentScan("com.example.demo") //by default what we are looking for
 @ComponentScan("com.example.componentscan")
 public class SpringIn5StepComponentScanApp {
@@ -17,7 +20,8 @@ public class SpringIn5StepComponentScanApp {
 
 	public static void main(String[] args) {
 
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepComponentScanApp.class, args);
+//		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepComponentScanApp.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepComponentScanApp.class);
 		ComponentDAO componentDAO = applicationContext.getBean(ComponentDAO.class);
 		LOG.info("{}", componentDAO);
 
--- src/main/java/com/example/demo/SpringIn5StepScopeApp.java
+++ src/main/java/com/example/demo/SpringIn5StepScopeApp.java
@@ -3,17 +3,22 @@ package com.example.demo;
 import com.example.demo.scope.PersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.example.demo")
 public class SpringIn5StepScopeApp {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepScopeApp.class);
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepScopeApp.class, args);
+//		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepScopeApp.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepScopeApp.class);
 		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
 		PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
 		LOGGER.info("{}", personDAO);
