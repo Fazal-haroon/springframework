095c69ab667771afd101007bb3ba4f49fcfe6852
Fazal Haroon <kharoon187@gmail.com>
2023-01-08 00:28:14 +0400

    XML Context with component scan for beans

--- src/main/java/com/example/demo/SpringIn5StepXMLContextApp.java
+++ src/main/java/com/example/demo/SpringIn5StepXMLContextApp.java
@@ -1,27 +1,23 @@
 package com.example.demo;
 
-import com.example.demo.basic.BinarySearchImpl;
 import com.example.demo.xml.XmlPersonDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//@SpringBootApplication
-@Configuration
-@ComponentScan
+
 public class SpringIn5StepXMLContextApp {
 
 	private static Logger logger = LoggerFactory.getLogger(SpringIn5StepXMLContextApp.class);
 
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")){
+			logger.info("Beans First Loaded -> {}", applicationContext.getBeanDefinitionNames());
+			logger.info("Beans List Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
 			XmlPersonDao bean = applicationContext.getBean(XmlPersonDao.class);
 			System.out.println("bean = " + bean);
 			System.out.println("bean.getXmlJdbcConn = " + bean.getXmlJdbcConnection());
-			logger.info("{}  --  {}", bean, bean.getXmlJdbcConnection());
+			logger.info("Beans of XmlPersonDao -> {}  Beans of autowired JDBC ->  {}", bean, bean.getXmlJdbcConnection());
 		}
 	}
 
