309a08c1753eaf6816b29bb51e17aea92e4bfd88
Fazal Haroon <kharoon187@gmail.com>
2023-01-07 23:52:19 +0400

    spring App context using XML based 2

--- src/main/java/com/example/demo/SpringIn5StepXMLContextApp.java
+++ src/main/java/com/example/demo/SpringIn5StepXMLContextApp.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import com.example.demo.basic.BinarySearchImpl;
 import com.example.demo.xml.XmlPersonDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +13,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Configuration
 @ComponentScan
 public class SpringIn5StepXMLContextApp {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringIn5StepXMLContextApp.class);
+
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")){
 			XmlPersonDao bean = applicationContext.getBean(XmlPersonDao.class);
 			System.out.println("bean = " + bean);
 			System.out.println("bean.getXmlJdbcConn = " + bean.getXmlJdbcConnection());
+			logger.info("{}  --  {}", bean, bean.getXmlJdbcConnection());
 		}
 	}
 
