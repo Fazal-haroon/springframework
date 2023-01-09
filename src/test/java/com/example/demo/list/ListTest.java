afb79dc249b6e184f47a5a99f2a816817e0ee196
Fazal Haroon <kharoon187@gmail.com>
2023-01-09 23:33:09 +0400

    mockito test by using Mocking List Interface

---
+++ src/test/java/com/example/demo/list/ListTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.list;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    void simpleTest(){
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(3);
+        assertEquals(3, listMock.size());
+        assertEquals(3, listMock.size());
+        assertEquals(3, listMock.size());
+    }
+
+    @Test
+    void multipleReturns(){
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(3).thenReturn(2);
+        assertEquals(3, listMock.size());
+        assertEquals(2, listMock.size());
+        assertEquals(2, listMock.size());
+        assertEquals(2, listMock.size());
+    }
+
+    @Test
+    void specificParameters(){
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("SomeString");
+        assertEquals("SomeString", listMock.get(0));
+        assertEquals(null, listMock.get(1));
+    }
+
+    @Test
+    void genericParameters(){
+        List listMock = mock(List.class);
+        when(listMock.get(Mockito.anyInt())).thenReturn("SomeOtherString");
+        assertEquals("SomeOtherString", listMock.get(0));
+        assertEquals("SomeOtherString", listMock.get(1));
+    }
+
+}
