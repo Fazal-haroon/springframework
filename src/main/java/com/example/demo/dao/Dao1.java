9544df68197eee51580dcc53ce29923eae515392
Fazal Haroon <kharoon187@gmail.com>
2023-01-12 01:08:31 +0400

    Setting up AOP Example

---
+++ src/main/java/com/example/demo/dao/Dao1.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao1 {
+    public String retrieveSomething(){
+        return "Dao1";
+    }
+}
---
+++ src/main/java/com/example/demo/dao/Dao2.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao2 {
+    public String retrieveSomething(){
+        return "Dao2";
+    }
+}
