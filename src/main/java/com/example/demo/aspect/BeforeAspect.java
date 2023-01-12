cef728ec451005f10cfac34ed0568e3336e3053f
Fazal Haroon <kharoon187@gmail.com>
2023-01-12 12:46:53 +0400

    AOP Terminology - PointCut, expression , Advice, Aspect, weaver framework and JoinPoint

--- src/main/java/com/example/demo/aspect/BeforeAspect.java
+++ src/main/java/com/example/demo/aspect/BeforeAspect.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.aspectj.lang.annotation.Aspect;
 
 
-@Aspect
+@Aspect //the combination of pointcut and advice is called Aspect
 @Configuration
 public class BeforeAspect {
 
@@ -18,9 +18,9 @@ public class BeforeAspect {
     //define point cut here
     //execution(* PACKAGE.*.*(..)) -> first * means any return type
 //    @Before("com.example.demo.service.Business1.calculateSomething()")
-    @Before("execution(* com.example.demo.service.Business1.*(..))")
-    public void before(JoinPoint joinPoint){
-        //get details use JoinPoint
-        logger.info(" Intercepted Method Calls - {}", joinPoint );
+    //(Weaving and Weaver) a framework which implements it called weaver, the process of doing that is called viewing
+    @Before("execution(* com.example.demo.service.Business1.*(..))") //this will intercept only Business1, pointcut is expression which define for Business1
+    public void before(JoinPoint joinPoint){ //get details use JoinPoint, it is specific interception of a method call, specific execution instance.
+        logger.info(" Intercepted Method Calls - {}", joinPoint ); //this is called an advice, what should i do when i do the interception
     }
 }
