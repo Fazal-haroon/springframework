c5f5479fe4b1b8ed2472be699c4d808a6596c861
Fazal Haroon <kharoon187@gmail.com>
2023-01-15 17:42:13 +0400

    implementing insert and update spring jdbc update methods

--- src/main/java/com/example/demo/DemoJDBCApplication.java
+++ src/main/java/com/example/demo/DemoJDBCApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import com.example.demo.jdbc.dao.PersonJdbcDao;
+import com.example.demo.jdbc.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DemoJDBCApplication implements CommandLineRunner {
@@ -28,5 +31,8 @@ public class DemoJDBCApplication implements CommandLineRunner {
 		logger.info("Find Person by location 'Islamabad' : {}", personJdbcDao.findByLocation("Islamabad"));
 		logger.info("Delete Person by Id '4' : {}", personJdbcDao.deleteById(4));
 		logger.info("Delete Person by Id '3' : {}", personJdbcDao.deleteById(3));
+		logger.info("Inserting 5th person : {}", personJdbcDao.insert(new Person(5, "Abdul Hanan", "Malakand", new Date())));
+		//INSERT INTO PERSON (ID, PERSON_NAME, LOCATION, BIRTH_DATE ) VALUES(2, 'Haroon', 'Islamabad', NOW());
+		logger.info("Updating 2th person : {}", personJdbcDao.update(new Person(2, "Fazal Haroon", "Batkhela Malakand", new Date())));
 	}
 }
--- src/main/java/com/example/demo/jdbc/dao/PersonJdbcDao.java
+++ src/main/java/com/example/demo/jdbc/dao/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -39,4 +40,16 @@ public class PersonJdbcDao {
     public int deleteById(int id){
         return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
     }
+
+    //INSERT INTO PERSON(ID, PERSON_NAME, LOCATION, BIRTH_DATE) VALUES(?, ?, ?, ?);
+    public int insert(Person person){
+        return jdbcTemplate.update("insert into person(id, person_name, location, birth_date) values(?, ?, ?, ?)",
+                new Object[]{person.getId(), person.getPersonName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime())});
+    }
+
+    public int update(Person person){
+        return jdbcTemplate.update("update person set person_name = ?, location = ?, birth_date = ? where id = ?",
+                new Object[]{person.getPersonName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId()});
+    }
+
 }
